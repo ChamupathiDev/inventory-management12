77d7d1aea509b602e7d84cafbfcdba6a14cd74cf
Dineth Chamupathi Kumara <127776710+ChamupathiDev@users.noreply.github.com>
2025-04-03 20:15:10 +0530

    inventorymodel created

--- backend/src/main/java/backend/model/InventoryModel.java
+++ backend/src/main/java/backend/model/InventoryModel.java
@@ -11,8 +11,68 @@ public class InventoryModel {
     private Long id;
     private String itemId;
     private String itemImage;
-    private String itemCstegory;
+    private String itemCategory;
     private String itemQty;
     private String itemDetails;
 
+    public InventoryModel(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemImage() {
+        return itemImage;
+    }
+
+    public void setItemImage(String itemImage) {
+        this.itemImage = itemImage;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+
+    public String getItemQty() {
+        return itemQty;
+    }
+
+    public void setItemQty(String itemQty) {
+        this.itemQty = itemQty;
+    }
+
+    public String getItemDetails() {
+        return itemDetails;
+    }
+
+    public void setItemDetails(String itemDetails) {
+        this.itemDetails = itemDetails;
+    }
+
+    public InventoryModel(Long id, String itemId, String itemImage, String itemCategory, String itemQty, String itemDetails) {
+        this.id = id;
+        this.itemId = itemId;
+        this.itemImage = itemImage;
+        this.itemCategory = itemCategory;
+        this.itemQty = itemQty;
+        this.itemDetails = itemDetails;
+    }
 }
