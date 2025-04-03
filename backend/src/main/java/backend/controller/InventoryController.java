b92e1085eb08fc54b0f6eb5390dfb7235c81770e
Dineth Chamupathi Kumara <127776710+ChamupathiDev@users.noreply.github.com>
2025-04-03 20:57:37 +0530

    changes

--- backend/src/main/java/backend/controller/InventoryController.java
+++ backend/src/main/java/backend/controller/InventoryController.java
@@ -1,12 +1,43 @@
 package backend.controller;
 
+import backend.model.InventoryModel;
+import backend.repository.InventoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+
 
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class InventoryController {
     @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @PostMapping("/inventory")
+    public InventoryModel newInventoryModel(@RequestBody InventoryModel newInventoryModel ){
+        return inventoryRepository.save(newInventoryModel);
+    }
+
+    @PostMapping("/inventory/itemImg")
+    public String itemImage(@RequestParam("file") MultipartFile file){
+        String folder = "src/main/uploads/";
+        String itemImage = file.getOriginalFilename();
+
+        try{
+            File uploadDir = new File(folder);
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+            file.transferTo(Paths.get(folder+itemImage));
+        }catch (IOException e){
+            e.printStackTrace();
+            return "Error uploading file;" + itemImage;
+        }
+        return itemImage;
+    }
 
 }
